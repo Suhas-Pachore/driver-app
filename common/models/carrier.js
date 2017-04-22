bc01a9f4d41f12a803b24a8b8cfbbc22daa0b8b8
Suhas Pachore <Suhas Pachore>
2017-04-22 16:57:17 +0530

    db query in remote method

--- common/models/carrier.js
+++ common/models/carrier.js
@@ -1,16 +1,19 @@
 'use strict';
+var loopback = require('loopback');
 
 module.exports = function(Carrier) {
 
     Carrier.getRoundtripLoads = function(msg, cb){
-        var roundTrips = [];
-
-        cb(null, '{"msg":"getting suggestions"}');
+        //var roundTrips = [];
+        var load = loopback.findModel('Load');
+        load.find(undefined, function(error, roundTrips){
+            cb(error,roundTrips);
+        });
+        //cb(null, '{"msg":"getting suggestions"}');
     }
 
      Carrier.getNearbyLoads = function(msg, cb){
         var nearbyLoads = [];
-        
         cb(null, '{"msg":"getting suggestions"}');
     }
 
