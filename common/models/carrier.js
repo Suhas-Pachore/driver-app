cc7a41a577a5a63e2740725bd6ded42fee006cff
Suhas Pachore <Suhas Pachore>
2017-04-22 16:42:30 +0530

    Carrier remote methods to get Loads

--- common/models/carrier.js
+++ common/models/carrier.js
@@ -2,4 +2,28 @@
 
 module.exports = function(Carrier) {
 
+    Carrier.getRoundtripLoads = function(msg, cb){
+        var roundTrips = [];
+
+        cb(null, '{"msg":"getting suggestions"}');
+    }
+
+     Carrier.getNearbyLoads = function(msg, cb){
+        var nearbyLoads = [];
+        
+        cb(null, '{"msg":"getting suggestions"}');
+    }
+
+    Carrier.remoteMethod('getRoundtripLoads',{
+        accepts: {arg: 'msg', type:'Object'},
+        returns: {arg: 'roundtripLoads', type:'array'}
+    });
+
+    Carrier.remoteMethod('getNearbyLoads',{
+        accepts: {arg: 'msg', type:'Object'},
+        returns: {arg: 'nearbyLoads', type:'array'}
+    });
+
+
+
 };
