b90e2fed553e3939d6e4d0889c434a50e1b3d413
NNG <NNG>
2017-04-23 07:06:44 +0530

    Created new model

---
+++ common/data/favouritelocation.js
@@ -0,0 +1,12 @@
+[ 
+   {
+    "CarrierId": "car001",
+    "Latitude": "1.1",
+    "Longitude": "1.2"
+   },
+   {
+    "CarrierId": "car002",
+    "Latitude": "2.1",
+    "Longitude": "3.2"
+   }
+ ]
